e0f9d739227b4b694fe198883d63f1313bc4cd04
Kunal goswami <71844891+kunal2013@users.noreply.github.com>
2021-09-17 21:20:20 +0530

    Add files via upload

---
+++ 11.break.cpp
@@ -0,0 +1,18 @@
+//
+
+# include <iostream>
+using namespace  std ;
+
+int main()
+{
+    int i ;
+    for (i=1; i<40 ; i++)
+    {
+        if (i ==5 )
+            continue ;
+       cout << i << endl ;
+    }
+        return 0 ;
+}
+
+
